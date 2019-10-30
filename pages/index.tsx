e36fefffc24caff517490d878445483c6c4c5009
James Gray <jamesbriangray@gmail.com>
2019-10-30 18:37:06 +0000

    Intial Commit

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+import React from 'react';
+
+const Home = () => (
+    <div>
+      <Head>
+        <title>Home</title>
+        <link rel='icon' href='../public/favicon.ico'/>
+      </Head>
+      <div className='hero'>
+        James Brian Gray
+      </div>
+    </div>
+);
+
+export default Home;
