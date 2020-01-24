79f51d7c5eacef152eb31046b90d14a99afa757e
James Gray <jamesbriangray@gmail.com>
2020-01-24 22:26:49 +0000

    static directory

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ const Home = () => {
         <Head>
           <title>James Gray Software Engineer</title>
           <meta name="description" content={'James Gray Software Engineer portfolio website!'} />
-          <link rel='icon' href='../../public/favicon.ico'/>
+          <link rel='icon' href='../../static/favicon.ico'/>
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css"
                 integrity="sha256-WAgYcAck1C1/zEl5sBl5cfyhxtLgKGdpI3oKyJffVRI=" crossOrigin="anonymous"/>
           <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet"/>
